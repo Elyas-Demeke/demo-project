20b52a1add4490de351e11aa1a3df308d2ff0a55
Elyas-Demeke <nebiyou.elyas11@gmail.com>
2024-01-04 03:53:41 +0300

    search suggestion and view all added

--- components/NavHeader.vue
+++ components/NavHeader.vue
@@ -1,11 +1,17 @@
 <template>
     <div class="flex w-full md:px-16 px-6 py-10 max-md:justify-between">
         <Brand/>
-        <div class="rounded-full border md:flex py-2.5 px-4 gap-x-4 flex-grow mr-8 hidden">
+        <div class="rounded-full border md:flex py-2.5 px-4 gap-x-4 flex-grow mr-8 hidden relative">
             <button @click="searchCar">
                 <img src="~/assets/icons/search-normal.svg" alt=""/>
             </button>
-            <input v-model="search" @keyup.enter="searchCar" type="text" class="outline-none leading-5 w-full" placeholder="Search something here">
+            <input v-model="search" @keyup.enter="searchCar" type="text" class="outline-none leading-5 w-full" placeholder="Search something here" @focus="focusedOnSearch = true" @blur="unFocus">
+            <div v-if="suggestedCars.length && focusedOnSearch" class="absolute flex flex-col translate-y-10 bg-white w-full rounded-md shadow-md p-4 gap-y-4">
+                <div @click="$router.push(`/car/${car.id}`)" v-for="(car, index) in suggestedCars" :key="car.id" class="flex py-3 gap-x-3 items-center cursor-pointer" :class="{'border-b': index < suggestedCars.length -1 }">
+                    <img :src="car.img" class="w-20 object-contain" alt="">
+                    <p>{{ car.name }}</p>
+                </div>
+            </div>
         </div>
         <button class="p-2.5 border rounded-full">
             <img src="~/assets/icons/heart.svg" alt=""/>
@@ -20,7 +26,13 @@
 </template>
 
 <script setup>
+import { debounce } from 'lodash';   
+
 const search = ref('')
+const suggestedCars = ref([])
+
+const focusedOnSearch = ref(false)
+
 onMounted(() => {
     const route = useRoute()
     search.value = route.query.q ? route.query.q: ''
@@ -30,6 +42,24 @@ const searchCar = () => {
     if(search.value) 
     useRouter().push(`/search?q=${search.value}`)
 }
+
+const suggestSearch = async() => {
+    if(search.value.length > 1){
+        const res = await $fetch(`/api/cars?q=${search.value}`)
+        suggestedCars.value = res.data
+    }else
+    suggestedCars.value = []
+}
+
+const unFocus = () => {
+    setTimeout(() => focusedOnSearch.value = false, 500)
+}
+
+
+watch(search, debounce(() => {
+	suggestSearch()
+}, 500))
+
 </script>
 
 <style scoped>
--- pages/search.vue
+++ pages/search.vue
@@ -2,6 +2,7 @@
     <div class="flex flex-col md:px-14 px-6 bg-blue-50 pt-8">
         <div class="flex justify-between py-4 mb-5 text-[#90A3BF]">
             <h3>Search results</h3>
+            <NuxtLink class="text-blue-500" to="/">View All</NuxtLink>
         </div>
         <div v-if="searchedCars" class="flex flex-wrap gap-8 mb-8">
             <CarCard v-if="searchedCars.length" class="max-w-none" v-for="car in searchedCars" :car="car" />
